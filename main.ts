fc4cbcc8b8579bbe1dcd830d7bf6bef8bd94fa16
419204 <115142418+419204@users.noreply.github.com>
2022-12-01 16:44:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,49 @@
+let list = [
+[
+1,
+0,
+1,
+0,
+1
+],
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+1,
+0,
+0,
+0,
+1
+],
+[
+1,
+0,
+0,
+0,
+1
+],
+[
+1,
+0,
+0,
+0,
+1
+]
+]
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        if (list[y][x] == 1) {
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+        }
+    }
+}
+basic.forever(function () {
+	
+})
